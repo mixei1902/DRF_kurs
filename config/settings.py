0ed7dca297f07cb9e5117808ec27e1ec17145182
mixei1902 <misei190289@gmail.com>
2024-08-29 18:52:22 +0300

    fix settings

--- config/settings.py
+++ config/settings.py
@@ -114,8 +114,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOWED_ORIGINS = [
     "http://localhost",
 ]
-CELERY_BROKER_URL = "redis://localhost:6379/0"
-CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
 
 CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 
